b98ebf42d6631f552270c48cb12f41eab289d904
Goran Sustek <goransustek@Gorans-MacBook-Pro.local>
2024-10-12 02:07:53 +0200

    Made changes to fix personal issue for special interactive_slider usecase

--- lib/interactive_slider_controller.dart
+++ lib/interactive_slider_controller.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/foundation.dart';
 
 class InteractiveSliderController extends ValueNotifier<double> {
-  InteractiveSliderController(super._value);
+  InteractiveSliderController(super.initialValue);
 
-  @override
-  set value(double value) => super.value = value.clamp(0.0, 1.0);
+  /// Set the slider value, but only allow it to change when opacity is less than 1.
+  void setValueWithOpacity(double newValue, double opacity) {
+    if (opacity < 1) {
+      value = newValue.clamp(0.0, 1.0); // The value must be between 0.0 and 1.0
+    }
+  }
 }
